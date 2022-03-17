90566759644411bd98d487c7f198d8ff292a9a69
Vais Bilyalov <blackpf@rambler.ru>
2022-03-17 20:10:27 +0300

    добавление форма создания рецепта

--- server.py
+++ server.py
@@ -7,6 +7,11 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+#@app.route('recept')
+#def recept():
+ #   return render_template('your_recept.html')
+
+
 @app.route('/user/<string:name>/<int:id>')
 def user(name, id):
     return "User page:" + name +" - " + str(id)
