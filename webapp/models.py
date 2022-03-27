7bcf735cce05a1f1edb2ce58fba260c695db0704
Ilya Galaktionov <igalaktionov1@gmail.com>
2022-03-27 17:53:03 +0300

    Исправлена ошибка

--- webapp/models.py
+++ webapp/models.py
@@ -23,6 +23,10 @@ class User(Base, UserMixin):
     def check_password(self, password):
         return check_password_hash(self.password, password)
 
+    @property
+    def is_admin(self):
+        return self.role == 'admin'
+
     def __repr__(self):
         return f"User {self.id} {self.username}"
 
