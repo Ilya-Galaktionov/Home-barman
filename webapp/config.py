5a710fff1c583b2681ef548fd8846738dd28e22d
Ilya Galaktionov <igalaktionov1@gmail.com>
2022-04-26 21:04:40 +0300

    удален файл config.py

--- webapp/config.py
+++
@@ -1,9 +0,0 @@
-from datetime import timedelta
-
-
-SQLALCHEMY_DATABASE_URI = 'postgresql://rueicqqh:hYlexQ-widfhtrmVt2vQMAbGHWRZz339@balarama.db.elephantsql.com:5432/rueicqqh'
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-SECRET_KEY = '35436656345gsdfgeMABf:UI&duwfrg'
-
-REMEMBER_COOKIE_DURATION = timedelta(days=5)
